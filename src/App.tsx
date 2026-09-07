974f2c5fcde80d7362923e18d46d95db57580b77
Kyle Austad <kyleaustad@gmail.com>
2026-09-07 18:30:39 -0500

    Add tailwind and shadcn for frontend UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
 import "./App.css";
+import { Input } from "./components/ui/input";
+import { Card } from "./components/ui/card";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [serverUrl, setServerUrl] = useState("");
@@ -22,18 +25,23 @@ function App() {
   }, []);
 
   return (
-    <main className="container">
-      <h1>{`Welcome to RomM-en`}</h1>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="serverUrl">Server URL</label>
-        <input
-          type="text"
-          value={serverUrl}
-          onChange={(e) => setServerUrl(e.target.value)}
-        />
+    <main
+      className="container dark h-screen min-w-full flex flex-col items-center justify-center "
+      style={{ backgroundColor: "var(--background)" }}
+    >
+      <h1 className="text-2xl text-foreground font-bold">{`Welcome to RomM-en`}</h1>
+      <div className="flex flex-col gap-2 items-center justify-center h-full w-full">
+        <Card className="flex flex-col gap-2 w-full max-w-md p-4">
+          <label htmlFor="serverUrl">Server URL</label>
+          <Input
+            type="text"
+            value={serverUrl}
+            onChange={(e) => setServerUrl(e.target.value)}
+          />
 
-        <button onClick={handleAuthenticate}>Authenticate</button>
-        {isAuthenticated && <p>Authenticated!</p>}
+          <Button onClick={handleAuthenticate}>Authenticate</Button>
+          {isAuthenticated && <p>Authenticated!</p>}
+        </Card>
       </div>
     </main>
   );
