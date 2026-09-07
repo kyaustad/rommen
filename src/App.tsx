72ea80fdd2c6f79b88987f99ffd4e916f791cd2d
Kyle Austad <kyleaustad@gmail.com>
2026-09-06 22:49:13 -0500

    update tauri event listener on auth success

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { invoke } from "@tauri-apps/api/core";
+import { listen } from "@tauri-apps/api/event";
 import "./App.css";
 
 function App() {
   const [serverUrl, setServerUrl] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   async function handleAuthenticate() {
     await invoke("authenticate", {
@@ -12,9 +14,16 @@ function App() {
     });
   }
 
+  useEffect(() => {
+    listen("auth_success", () => {
+      console.log("Authentication successful");
+      setIsAuthenticated(true);
+    });
+  }, []);
+
   return (
     <main className="container">
-      <h1>Welcome to Rommen</h1>
+      <h1>{`Welcome to RomM-en`}</h1>
       <div className="flex flex-col gap-2">
         <label htmlFor="serverUrl">Server URL</label>
         <input
@@ -24,6 +33,7 @@ function App() {
         />
 
         <button onClick={handleAuthenticate}>Authenticate</button>
+        {isAuthenticated && <p>Authenticated!</p>}
       </div>
     </main>
   );
